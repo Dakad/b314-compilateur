fcc5244e419a7637b8cbf1eab268d9e9a0b566cc
Dakad <bpottered@outlook.com>
2018-03-02 17:59:16 +0100

    Regenere test unit

--- src/test/java/be/unamur/info/b314/compiler/main/B314importSyntaxTest.java
+++ src/test/java/be/unamur/info/b314/compiler/main/B314importSyntaxTest.java
@@ -38,16 +38,6 @@ public class B314importSyntaxTest {
     // Serie import KO
     //
     @Test
-    public void testimport_missing_file_ext_ko() throws Exception {
-        CompilerTestHelper.launchCompilation("/syntax/import/ko/missing_file_ext.b314", testFolder.newFile(), false, "import: missing_file_ext");
-    }
-
-    @Test
-    public void testimport_missing_import_ko() throws Exception {
-        CompilerTestHelper.launchCompilation("/syntax/import/ko/missing_import.b314", testFolder.newFile(), false, "import: missing_import");
-    }
-
-    @Test
     public void testimport_double_import_ko() throws Exception {
         CompilerTestHelper.launchCompilation("/syntax/import/ko/double_import.b314", testFolder.newFile(), false, "import: double_import");
     }
@@ -55,6 +45,16 @@ public class B314importSyntaxTest {
     @Test
     public void testimport_missing_filename_ko() throws Exception {
         CompilerTestHelper.launchCompilation("/syntax/import/ko/missing_filename.b314", testFolder.newFile(), false, "import: missing_filename");
+    }
+
+    @Test
+    public void testimport_missing_file_ext_ko() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/import/ko/missing_file_ext.b314", testFolder.newFile(), false, "import: missing_file_ext");
+    }
+
+    @Test
+    public void testimport_missing_import_ko() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/import/ko/missing_import.b314", testFolder.newFile(), false, "import: missing_import");
     }
 
     @Test
