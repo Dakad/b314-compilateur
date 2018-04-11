c5634385f8ebf9fff7bb357834da0466bbef3f3b
kadegnon <kodjo.adegnon@student.unamur.be>
2018-04-11 05:41:24 +0200

    Ajout des overrid de methodes pour les Visit et SymbTableFiller

--- src/main/java/be/unamur/info/b314/compiler/main/PCodeVisitor.java
+++ src/main/java/be/unamur/info/b314/compiler/main/PCodeVisitor.java
@@ -1,8 +1,35 @@
 package be.unamur.info.b314.compiler.main;
 
 import be.unamur.info.b314.compiler.B314BaseVisitor;
+import be.unamur.info.b314.compiler.B314Parser.ActionContext;
+import be.unamur.info.b314.compiler.B314Parser.ArrayContext;
+import be.unamur.info.b314.compiler.B314Parser.BoardContext;
+import be.unamur.info.b314.compiler.B314Parser.BoolValContext;
+import be.unamur.info.b314.compiler.B314Parser.ClauseDefaultContext;
+import be.unamur.info.b314.compiler.B314Parser.ClauseWhenContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprBoolContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprCaseContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprDContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprDFctContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprGContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprIntContext;
+import be.unamur.info.b314.compiler.B314Parser.FctDeclContext;
+import be.unamur.info.b314.compiler.B314Parser.FileDeclContext;
 import be.unamur.info.b314.compiler.B314Parser.ImpDeclContext;
+import be.unamur.info.b314.compiler.B314Parser.InstrContext;
+import be.unamur.info.b314.compiler.B314Parser.IntValContext;
+import be.unamur.info.b314.compiler.B314Parser.OpBoolCompareContext;
+import be.unamur.info.b314.compiler.B314Parser.OpBoolContext;
+import be.unamur.info.b314.compiler.B314Parser.OpIntContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramMondeContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramMondeGlobalDeclContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramStratContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramStratGlobalDeclContext;
 import be.unamur.info.b314.compiler.B314Parser.RootContext;
+import be.unamur.info.b314.compiler.B314Parser.ScalarContext;
+import be.unamur.info.b314.compiler.B314Parser.TypeContext;
+import be.unamur.info.b314.compiler.B314Parser.VarDeclContext;
 import java.util.Map;
 
 /**
@@ -75,4 +102,139 @@ public class PCodeVisitor extends B314BaseVisitor<Object> {
     return null;
   }*/
 
+
+  @Override
+  public Object visitType(TypeContext ctx) {
+    return super.visitType(ctx);
+  }
+
+  @Override
+  public Object visitScalar(ScalarContext ctx) {
+    return super.visitScalar(ctx);
+  }
+
+  @Override
+  public Object visitArray(ArrayContext ctx) {
+    return super.visitArray(ctx);
+  }
+
+  @Override
+  public Object visitBoard(BoardContext ctx) {
+    return super.visitBoard(ctx);
+  }
+
+  @Override
+  public Object visitVarDecl(VarDeclContext ctx) {
+    return super.visitVarDecl(ctx);
+  }
+
+  @Override
+  public Object visitFileDecl(FileDeclContext ctx) {
+    return super.visitFileDecl(ctx);
+  }
+
+  @Override
+  public Object visitAction(ActionContext ctx) {
+    return super.visitAction(ctx);
+  }
+
+  @Override
+  public Object visitIntVal(IntValContext ctx) {
+    return super.visitIntVal(ctx);
+  }
+
+  @Override
+  public Object visitOpInt(OpIntContext ctx) {
+    return super.visitOpInt(ctx);
+  }
+
+  @Override
+  public Object visitBoolVal(BoolValContext ctx) {
+    return super.visitBoolVal(ctx);
+  }
+
+  @Override
+  public Object visitOpBool(OpBoolContext ctx) {
+    return super.visitOpBool(ctx);
+  }
+
+  @Override
+  public Object visitOpBoolCompare(OpBoolCompareContext ctx) {
+    return super.visitOpBoolCompare(ctx);
+  }
+
+  @Override
+  public Object visitExprDFct(ExprDFctContext ctx) {
+    return super.visitExprDFct(ctx);
+  }
+
+  @Override
+  public Object visitExprD(ExprDContext ctx) {
+    return super.visitExprD(ctx);
+  }
+
+  @Override
+  public Object visitExprInt(ExprIntContext ctx) {
+    return super.visitExprInt(ctx);
+  }
+
+  @Override
+  public Object visitExprBool(ExprBoolContext ctx) {
+    return super.visitExprBool(ctx);
+  }
+
+  @Override
+  public Object visitExprCase(ExprCaseContext ctx) {
+    return super.visitExprCase(ctx);
+  }
+
+  @Override
+  public Object visitExprG(ExprGContext ctx) {
+    return super.visitExprG(ctx);
+  }
+
+  @Override
+  public Object visitFctDecl(FctDeclContext ctx) {
+    return super.visitFctDecl(ctx);
+  }
+
+  @Override
+  public Object visitInstr(InstrContext ctx) {
+    return super.visitInstr(ctx);
+  }
+
+  @Override
+  public Object visitProgram(ProgramContext ctx) {
+    return super.visitProgram(ctx);
+  }
+
+  @Override
+  public Object visitProgramMonde(ProgramMondeContext ctx) {
+    return super.visitProgramMonde(ctx);
+  }
+
+  @Override
+  public Object visitProgramMondeGlobalDecl(ProgramMondeGlobalDeclContext ctx) {
+    return super.visitProgramMondeGlobalDecl(ctx);
+  }
+
+  @Override
+  public Object visitProgramStrat(ProgramStratContext ctx) {
+    return super.visitProgramStrat(ctx);
+  }
+
+  @Override
+  public Object visitProgramStratGlobalDecl(ProgramStratGlobalDeclContext ctx) {
+    return super.visitProgramStratGlobalDecl(ctx);
+  }
+
+  @Override
+  public Object visitClauseDefault(ClauseDefaultContext ctx) {
+    return super.visitClauseDefault(ctx);
+  }
+
+  @Override
+  public Object visitClauseWhen(ClauseWhenContext ctx) {
+    return super.visitClauseWhen(ctx);
+  }
 }
--- src/main/java/be/unamur/info/b314/compiler/main/SymTableFiller.java
+++ src/main/java/be/unamur/info/b314/compiler/main/SymTableFiller.java
@@ -1,8 +1,38 @@
 package be.unamur.info.b314.compiler.main;
 
 import be.unamur.info.b314.compiler.B314BaseListener;
+import be.unamur.info.b314.compiler.B314Parser.ActionContext;
+import be.unamur.info.b314.compiler.B314Parser.ArrayContext;
+import be.unamur.info.b314.compiler.B314Parser.BoardContext;
+import be.unamur.info.b314.compiler.B314Parser.BoolValContext;
+import be.unamur.info.b314.compiler.B314Parser.ClauseDefaultContext;
+import be.unamur.info.b314.compiler.B314Parser.ClauseWhenContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprBoolContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprCaseContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprDContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprDFctContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprGContext;
+import be.unamur.info.b314.compiler.B314Parser.ExprIntContext;
+import be.unamur.info.b314.compiler.B314Parser.FctDeclContext;
+import be.unamur.info.b314.compiler.B314Parser.FileDeclContext;
+import be.unamur.info.b314.compiler.B314Parser.ImpDeclContext;
+import be.unamur.info.b314.compiler.B314Parser.InstrContext;
+import be.unamur.info.b314.compiler.B314Parser.IntValContext;
+import be.unamur.info.b314.compiler.B314Parser.OpBoolCompareContext;
+import be.unamur.info.b314.compiler.B314Parser.OpBoolContext;
+import be.unamur.info.b314.compiler.B314Parser.OpIntContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramMondeContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramMondeGlobalDeclContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramStratContext;
+import be.unamur.info.b314.compiler.B314Parser.ProgramStratGlobalDeclContext;
 import be.unamur.info.b314.compiler.B314Parser.RootContext;
 
+import be.unamur.info.b314.compiler.B314Parser.ScalarContext;
+import be.unamur.info.b314.compiler.B314Parser.TypeContext;
+import be.unamur.info.b314.compiler.B314Parser.VarDeclContext;
+import org.antlr.symtab.GlobalScope;
+import org.antlr.symtab.PrimitiveType;
 import org.antlr.symtab.Scope;
 import org.antlr.symtab.SymbolTable;
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -32,6 +62,13 @@ public class SymTableFiller extends B314BaseListener {
 
   public SymTableFiller() {
     this.symTable = new SymbolTable();
+    this.definePrimitiveType();
+  }
+
+  private void definePrimitiveType() {
+    this.symTable.definePredefinedSymbol(new PrimitiveType("boolean"));
+    this.symTable.definePredefinedSymbol(new PrimitiveType("integer"));
+    this.symTable.definePredefinedSymbol(new PrimitiveType("square"));
   }
 
 
@@ -52,12 +89,13 @@ public class SymTableFiller extends B314BaseListener {
 
   @Override
   public void enterRoot(RootContext ctx) {
-    super.enterRoot(ctx);
+    GlobalScope gs = symTable.GLOBALS;
+    pushScope(gs);
   }
 
   @Override
   public void exitRoot(RootContext ctx) {
-    super.exitRoot(ctx);
+    popScope();
   }
 
   @Override
@@ -72,6 +110,7 @@ public class SymTableFiller extends B314BaseListener {
 
   @Override
   public void visitTerminal(TerminalNode node) {
+    node.
     super.visitTerminal(node);
   }
 
@@ -80,4 +119,303 @@ public class SymTableFiller extends B314BaseListener {
     super.visitErrorNode(node);
   }
 
+  @Override
+  public void enterType(TypeContext ctx) {
+    super.enterType(ctx);
+  }
+
+  @Override
+  public void exitType(TypeContext ctx) {
+    super.exitType(ctx);
+  }
+
+  @Override
+  public void enterScalar(ScalarContext ctx) {
+    super.enterScalar(ctx);
+  }
+
+  @Override
+  public void exitScalar(ScalarContext ctx) {
+    super.exitScalar(ctx);
+  }
+
+  @Override
+  public void enterArray(ArrayContext ctx) {
+    super.enterArray(ctx);
+  }
+
+  @Override
+  public void exitArray(ArrayContext ctx) {
+    super.exitArray(ctx);
+  }
+
+  @Override
+  public void enterBoard(BoardContext ctx) {
+    super.enterBoard(ctx);
+  }
+
+  @Override
+  public void exitBoard(BoardContext ctx) {
+    super.exitBoard(ctx);
+  }
+
+  @Override
+  public void enterVarDecl(VarDeclContext ctx) {
+    super.enterVarDecl(ctx);
+  }
+
+  @Override
+  public void exitVarDecl(VarDeclContext ctx) {
+    super.exitVarDecl(ctx);
+  }
+
+  @Override
+  public void enterImpDecl(ImpDeclContext ctx) {
+    super.enterImpDecl(ctx);
+  }
+
+  @Override
+  public void exitImpDecl(ImpDeclContext ctx) {
+    super.exitImpDecl(ctx);
+  }
+
+  @Override
+  public void enterFileDecl(FileDeclContext ctx) {
+    super.enterFileDecl(ctx);
+  }
+
+  @Override
+  public void exitFileDecl(FileDeclContext ctx) {
+    super.exitFileDecl(ctx);
+  }
+
+  @Override
+  public void enterAction(ActionContext ctx) {
+    super.enterAction(ctx);
+  }
+
+  @Override
+  public void exitAction(ActionContext ctx) {
+    super.exitAction(ctx);
+  }
+
+  @Override
+  public void enterIntVal(IntValContext ctx) {
+    super.enterIntVal(ctx);
+  }
+
+  @Override
+  public void exitIntVal(IntValContext ctx) {
+    super.exitIntVal(ctx);
+  }
+
+  @Override
+  public void enterOpInt(OpIntContext ctx) {
+    super.enterOpInt(ctx);
+  }
+
+  @Override
+  public void exitOpInt(OpIntContext ctx) {
+    super.exitOpInt(ctx);
+  }
+
+  @Override
+  public void enterBoolVal(BoolValContext ctx) {
+    super.enterBoolVal(ctx);
+  }
+
+  @Override
+  public void exitBoolVal(BoolValContext ctx) {
+    super.exitBoolVal(ctx);
+  }
+
+  @Override
+  public void enterOpBool(OpBoolContext ctx) {
+    super.enterOpBool(ctx);
+  }
+
+  @Override
+  public void exitOpBool(OpBoolContext ctx) {
+    super.exitOpBool(ctx);
+  }
+
+  @Override
+  public void enterOpBoolCompare(OpBoolCompareContext ctx) {
+    super.enterOpBoolCompare(ctx);
+  }
+
+  @Override
+  public void exitOpBoolCompare(OpBoolCompareContext ctx) {
+    super.exitOpBoolCompare(ctx);
+  }
+
+  @Override
+  public void enterExprDFct(ExprDFctContext ctx) {
+    super.enterExprDFct(ctx);
+  }
+
+  @Override
+  public void exitExprDFct(ExprDFctContext ctx) {
+    super.exitExprDFct(ctx);
+  }
+
+  @Override
+  public void enterExprD(ExprDContext ctx) {
+    super.enterExprD(ctx);
+  }
+
+  @Override
+  public void exitExprD(ExprDContext ctx) {
+    super.exitExprD(ctx);
+  }
+
+  @Override
+  public void enterExprInt(ExprIntContext ctx) {
+    super.enterExprInt(ctx);
+  }
+
+  @Override
+  public void exitExprInt(ExprIntContext ctx) {
+    super.exitExprInt(ctx);
+  }
+
+  @Override
+  public void enterExprBool(ExprBoolContext ctx) {
+    super.enterExprBool(ctx);
+  }
+
+  @Override
+  public void exitExprBool(ExprBoolContext ctx) {
+    super.exitExprBool(ctx);
+  }
+
+  @Override
+  public void enterExprCase(ExprCaseContext ctx) {
+    super.enterExprCase(ctx);
+  }
+
+  @Override
+  public void exitExprCase(ExprCaseContext ctx) {
+    super.exitExprCase(ctx);
+  }
+
+  @Override
+  public void enterExprG(ExprGContext ctx) {
+    super.enterExprG(ctx);
+  }
+
+  @Override
+  public void exitExprG(ExprGContext ctx) {
+    super.exitExprG(ctx);
+  }
+
+  @Override
+  public void enterFctDecl(FctDeclContext ctx) {
+    super.enterFctDecl(ctx);
+  }
+
+  @Override
+  public void exitFctDecl(FctDeclContext ctx) {
+    super.exitFctDecl(ctx);
+  }
+
+  @Override
+  public void enterInstr(InstrContext ctx) {
+    super.enterInstr(ctx);
+  }
+
+  @Override
+  public void exitInstr(InstrContext ctx) {
+    super.exitInstr(ctx);
+  }
+
+  @Override
+  public void enterProgram(ProgramContext ctx) {
+    super.enterProgram(ctx);
+  }
+
+  @Override
+  public void exitProgram(ProgramContext ctx) {
+    super.exitProgram(ctx);
+  }
+
+  @Override
+  public void enterProgramMonde(ProgramMondeContext ctx) {
+    super.enterProgramMonde(ctx);
+  }
+
+  @Override
+  public void exitProgramMonde(ProgramMondeContext ctx) {
+    super.exitProgramMonde(ctx);
+  }
+
+  @Override
+  public void enterProgramMondeGlobalDecl(ProgramMondeGlobalDeclContext ctx) {
+    super.enterProgramMondeGlobalDecl(ctx);
+  }
+
+  @Override
+  public void exitProgramMondeGlobalDecl(ProgramMondeGlobalDeclContext ctx) {
+    super.exitProgramMondeGlobalDecl(ctx);
+  }
+
+  @Override
+  public void enterProgramStrat(ProgramStratContext ctx) {
+    super.enterProgramStrat(ctx);
+  }
+
+  @Override
+  public void exitProgramStrat(ProgramStratContext ctx) {
+    super.exitProgramStrat(ctx);
+  }
+
+  @Override
+  public void enterProgramStratGlobalDecl(ProgramStratGlobalDeclContext ctx) {
+    super.enterProgramStratGlobalDecl(ctx);
+  }
+
+  @Override
+  public void exitProgramStratGlobalDecl(ProgramStratGlobalDeclContext ctx) {
+    super.exitProgramStratGlobalDecl(ctx);
+  }
+
+  @Override
+  public void enterClauseDefault(ClauseDefaultContext ctx) {
+    super.enterClauseDefault(ctx);
+  }
+
+  @Override
+  public void exitClauseDefault(ClauseDefaultContext ctx) {
+    super.exitClauseDefault(ctx);
+  }
+
+  @Override
+  public void enterClauseWhen(ClauseWhenContext ctx) {
+    super.enterClauseWhen(ctx);
+  }
+
+  @Override
+  public void exitClauseWhen(ClauseWhenContext ctx) {
+    super.exitClauseWhen(ctx);
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
 }
