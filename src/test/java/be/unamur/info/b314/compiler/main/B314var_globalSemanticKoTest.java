5cdb2159d726733028d4f8f8e1c1b9caef93c140
kadegnon <kodjo.adegnon@student.unamur.be>
2018-04-29 20:20:02 +0200

    Ajust test unit sur semantiq de var global - Valid Exception

--- src/test/java/be/unamur/info/b314/compiler/main/B314var_globalSemanticKoTest.java
+++ src/test/java/be/unamur/info/b314/compiler/main/B314var_globalSemanticKoTest.java
@@ -1,6 +1,11 @@
 package be.unamur.info.b314.compiler.main;
 
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
 import be.unamur.info.b314.compiler.semantics.SymTableFiller;
+import be.unamur.info.b314.compiler.semantics.exception.AlreadyGloballyDeclared;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -12,29 +17,36 @@ import org.slf4j.LoggerFactory;
 
 public class B314var_globalSemanticKoTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(B314var_globalSemanticKoTest.class);
+  private static final Logger LOG = LoggerFactory.getLogger(B314var_globalSemanticKoTest.class);
 
-    @Rule
-    public TemporaryFolder testFolder = new TemporaryFolder(); // Create a temporary folder for outputs deleted after tests
+  @Rule
+  public TemporaryFolder testFolder = new TemporaryFolder(); // Create a temporary folder for outputs deleted after tests
 
-    @Rule
-    public TestRule watcher = new TestWatcher() { // Prints message on logger before each test
-        @Override
-        protected void starting(Description description) {
-            LOG.info(String.format("Starting test: %s()...",
-                    description.getMethodName()));
-        }
-    ;
-    };
-
-    //
-    // Serie var_global KO
-    //
-    @Test
-    public void testvar_global_duplicate_name_ko() throws Exception{
-        SymTableFiller symbtable = CompilerTestHelper
-            .getSymTable("/semantics/var_global/ko/duplicate_name.b314");
+  @Rule
+  public TestRule watcher = new TestWatcher() { // Prints message on logger before each test
+    @Override
+    protected void starting(Description description) {
+      LOG.info(String.format("Starting test: %s()...",
+          description.getMethodName()));
     }
+
+    ;
+  };
+
+  //
+  // Serie var_global KO
+  //
+  @Test
+  public void testvar_global_duplicate_name_ko() {
+    try {
+      CompilerTestHelper.getSymTable("/semantics/var_global/ko/duplicate_name.b314");
+
+      fail("[Unthrowed] This .b314 contains duplicate global variable");
+    } catch (RuntimeException e) {
+      assertThat("Incorrect type of Exception throwned", e,
+          instanceOf(AlreadyGloballyDeclared.class));
+    }
+  }
 
 
 }
