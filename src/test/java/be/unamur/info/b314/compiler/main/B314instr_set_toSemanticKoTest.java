eec605ae7d165dcc03b4e0efbe7ec55666115331
kadegnon <kodjo.adegnon@student.unamur.be>
2018-05-01 19:01:02 +0200

    Ajout test unit KO de semantiq pour instruction setTo

--- src/test/java/be/unamur/info/b314/compiler/main/B314instr_set_toSemanticKoTest.java
+++ src/test/java/be/unamur/info/b314/compiler/main/B314instr_set_toSemanticKoTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import be.unamur.info.b314.compiler.semantics.exception.NotMatchingType;
+import be.unamur.info.b314.compiler.semantics.exception.UndeclaredVariable;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -42,14 +43,34 @@ public class B314instr_set_toSemanticKoTest {
         "and", "or", "eq", "gt", "lge", "lt", "lte", "or", "par", "true", "false"
     };
 
-    for (String b314 : b314Files) {
+    for (String name : b314Files) {
       try {
-        String file = "/semantics/instr_set_to/ko/arena_case_to_boolean_" + b314;
-        CompilerTestHelper.getSymTable(file + ".b314");
+        String file = "/semantics/instr_set_to/ko/arena_case_to_boolean_" + name+ ".b314";
+        CompilerTestHelper.getSymTable(file);
         fail("[Unthrowed] This .b314 is invalid. Should have thrown an Exception.\n" + file);
       } catch (RuntimeException e) {
         assertThat("Incorrect type of Exception throwned", e,
             instanceOf(NotMatchingType.class));
+        assertThat("Must contain a detailed msg of the error", e.getMessage(), notNullValue());
+      }
+    }
+
+  }
+
+@Test
+  public void testinstr_set_to_undeclared_variable_ko() {
+    String[] b314Files = {
+        "var", "array", "array_2dim",
+    };
+
+    for (String b314 : b314Files) {
+      try {
+        String file = "/semantics/instr_set_to/ko/undeclared_" + b314 + ".b314";
+        CompilerTestHelper.getSymTable(file);
+        fail("[Unthrowed] This .b314 is invalid. Should have thrown an Exception.\n" + file);
+      } catch (RuntimeException e) {
+        assertThat("Incorrect type of Exception throwned", e,
+            instanceOf(UndeclaredVariable.class));
         assertThat("Must contain a detailed msg of the error", e.getMessage(), notNullValue());
       }
     }
