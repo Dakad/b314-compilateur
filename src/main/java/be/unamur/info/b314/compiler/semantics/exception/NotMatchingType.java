43eab4e66eb83e66a8754fb044ca242e662d8bca
Dakad <bpottered@outlook.com>
2018-05-01 17:16:52 +0200

    Ajout d'Exception pour semantique d'instruction 'set ... to ...'

--- src/main/java/be/unamur/info/b314/compiler/semantics/exception/NotMatchingType.java
+++ src/main/java/be/unamur/info/b314/compiler/semantics/exception/NotMatchingType.java
@@ -1,17 +1,8 @@
 package be.unamur.info.b314.compiler.semantics.exception;
 
-import be.unamur.info.b314.compiler.B314Parser.SetToContext;
-
-
 public class NotMatchingType extends RuntimeException {
 
   private static final String msg = "The type of the instruction is incorrect - Instr : ";
-
-  /**
-   * @effects
-   */
-  public NotMatchingType(SetToContext ctx) {
-  }
 
   /**
    * @effects
