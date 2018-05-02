9eed90858383bb21525077572a1ab1f287edef5f
kadegnon <kodjo.adegnon@student.unamur.be>
2018-05-02 14:55:58 +0200

    Genere test unit semantiq pr inst if-then-else

---
+++ src/test/java/be/unamur/info/b314/compiler/main/B314instr_if_then_elseSemanticOkTest.java
@@ -0,0 +1,62 @@
+package be.unamur.info.b314.compiler.main;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class B314instr_if_then_elseSemanticOkTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(B314instr_if_then_elseSemanticOkTest.class);
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder(); // Create a temporary folder for outputs deleted after tests
+
+    @Rule
+    public TestRule watcher = new TestWatcher() { // Prints message on logger before each test
+        @Override
+        protected void starting(Description description) {
+            LOG.info(String.format("Starting test: %s()...",
+                    description.getMethodName()));
+        }
+    ;
+    };
+
+    //
+    // Serie instr_if_then_else OK
+    //
+    @Test
+    public void testinstr_if_then_else_array_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/semantics/instr_if_then_else/ok/array.b314", testFolder.newFile(), true, "instr_if_then_else: array");
+    }
+
+    @Test
+    public void testinstr_if_then_else_and_or_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/semantics/instr_if_then_else/ok/and_or.b314", testFolder.newFile(), true, "instr_if_then_else: and_or");
+    }
+
+    @Test
+    public void testinstr_if_then_else_op_var_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/semantics/instr_if_then_else/ok/op_var.b314", testFolder.newFile(), true, "instr_if_then_else: op_var");
+    }
+
+    @Test
+    public void testinstr_if_then_else_true_false_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/semantics/instr_if_then_else/ok/true_false.b314", testFolder.newFile(), true, "instr_if_then_else: true_false");
+    }
+
+    @Test
+    public void testinstr_if_then_else_op_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/semantics/instr_if_then_else/ok/op.b314", testFolder.newFile(), true, "instr_if_then_else: op");
+    }
+
+    @Test
+    public void testinstr_if_then_else_var_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/semantics/instr_if_then_else/ok/var.b314", testFolder.newFile(), true, "instr_if_then_else: var");
+    }
+
+}
