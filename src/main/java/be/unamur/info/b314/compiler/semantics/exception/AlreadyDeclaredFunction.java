e842f966153a963c5a249e9d60590bee5df4eb59
kadegnon <kodjo.adegnon@student.unamur.be>
2018-05-03 20:15:24 +0200

    Gestion de fonctions simple

--- src/main/java/be/unamur/info/b314/compiler/semantics/exception/AlreadyDeclaredFunction.java
+++ src/main/java/be/unamur/info/b314/compiler/semantics/exception/AlreadyDeclaredFunction.java
@@ -2,13 +2,10 @@ package be.unamur.info.b314.compiler.semantics.exception;
 
 public class AlreadyDeclaredFunction extends RuntimeException {
 
-  private static final String msg = "A global variable has already been defined with the name : ";
+  private static final String msg = "A global function has already been defined with the name : ";
 
   public AlreadyDeclaredFunction(String name) {
     super(msg + name);
   }
 
-  public AlreadyDeclaredFunction() {
-    this("undefined");
-  }
 }
