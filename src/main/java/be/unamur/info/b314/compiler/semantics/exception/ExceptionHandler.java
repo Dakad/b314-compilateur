1bbaa2fe807a40f4a63537ffcac644e8b6c593f6
ARajaona <andryrajaona@hotmail.com>
2018-05-06 23:32:02 +0200

    ajout fonction throwNotReturnVoidFucntion dans ExceptionHandler

--- src/main/java/be/unamur/info/b314/compiler/semantics/exception/ExceptionHandler.java
+++ src/main/java/be/unamur/info/b314/compiler/semantics/exception/ExceptionHandler.java
@@ -16,6 +16,8 @@ public class ExceptionHandler {
   private static final String ERR_MSG_NOT_MATCHING_TYPE = "The type of the instruction is incorrect - Instr : ";
   private static final String ERR_MSG_UNDECLARED_VARIABLE = "Undeclared identifier : ";
   private static final String ERR_MSG_NOT_BOOLEAN_CONDITION = "The type of the condition statement is not boolean - Instr : ";
+  private static final String ERR_MSG_NOT_RETURN_VOID_FUNCTION  = "Function has not return VOID : ";
+
 
   /**
    *
@@ -190,6 +192,29 @@ public class ExceptionHandler {
   public static void throwNotBooleanCondition(ParserRuleContext ctx, String instr) {
     String formatMsg = formatterMsg(ctx, ERR_MSG_NOT_BOOLEAN_CONDITION)+instr;
     throw  new NotBooleanCondition(formatMsg);
+  }
+
+
+
+  /**
+   *
+   *@requires ctx - Context of the instruction causing the error. Must be not null
+   * @throws NotReturnVoidFucntion  with the pretty msg.
+   */
+  public static void throwNotReturnVoidFucntion (ParserRuleContext ctx) {
+    String formatMsg = formatterMsg(ctx, ERR_MSG_NOT_RETURN_VOID_FUNCTION);
+    throw  new NotReturnVoidFucntion (formatMsg);
+  }
+
+  /**
+   *
+   *@requires ctx - Context of the instruction causing the error. Must be not null
+   *@requires name - name function declared
+   * @throws NotReturnVoidFucntion  with the pretty msg.
+   */
+  public static void throwNotReturnVoidFucntion (ParserRuleContext ctx, String name) {
+    String formatMsg = formatterMsg(ctx, ERR_MSG_NOT_RETURN_VOID_FUNCTION)+name;
+    throw  new NotReturnVoidFucntion (formatMsg);
   }
 
 }
