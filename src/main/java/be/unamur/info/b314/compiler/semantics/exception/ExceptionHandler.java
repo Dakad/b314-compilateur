675f4bd289877a577f60b24b17ba3521aaa683aa
ARajaona <andryrajaona@hotmail.com>
2018-05-06 21:49:48 +0200

    ajout class ExceptionHandler

---
+++ src/main/java/be/unamur/info/b314/compiler/semantics/exception/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package be.unamur.info.b314.compiler.semantics.exception;
+
+/**
+ * @overview ExceptionHandler is an handler for thrown exception. It sole purpose is to thrown pretty Semantic Error.
+ * An ExceptionHandler is stateless, thus, immutable.
+ *
+ */
+public class ExceptionHandler {
+
+}
