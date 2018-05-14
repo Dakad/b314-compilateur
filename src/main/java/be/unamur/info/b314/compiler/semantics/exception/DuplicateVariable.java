a5d1bf34d0bb087eb525eafb9496d20567f78754
nazchris <christian.naziliwanlongo@student.unamur.be>
2018-05-14 15:04:02 +0200

    Ajust Exception Handler

--- src/main/java/be/unamur/info/b314/compiler/semantics/exception/DuplicateVariable.java
+++ src/main/java/be/unamur/info/b314/compiler/semantics/exception/DuplicateVariable.java
@@ -1,6 +1,6 @@
 package be.unamur.info.b314.compiler.semantics.exception;
 
-public class DuplicateVariable extends RuntimeException {
+public class DuplicateVariable extends SemanticException{
 
   public DuplicateVariable(String msgError) {
     super(msgError);
--- src/main/java/be/unamur/info/b314/compiler/semantics/exception/NotPositiveSizeForArray.java
+++ src/main/java/be/unamur/info/b314/compiler/semantics/exception/NotPositiveSizeForArray.java
@@ -1,6 +1,6 @@
 package be.unamur.info.b314.compiler.semantics.exception;
 
-public class NotPositiveSizeForArray extends RuntimeException {
+public class NotPositiveSizeForArray extends SemanticException{
 
   public NotPositiveSizeForArray(){
     super();
